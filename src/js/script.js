ee258079a9024134f6db80d66d886daf26abbc56
klarky1 <crosee77@gmail.com>
2025-07-09 18:33:22 +0500

    Made some code refactoring for script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 import CyrillicToTranslit from "cyrillic-to-translit-js";
 const cyrillicToTranslit = new CyrillicToTranslit();
 
-// DOM elements
+// ** DOM elements **
 const productForm = document.getElementById("product-form");
 const productTextInput = document.getElementById("product-text-input");
 const alertPopupForProductInput = productForm.querySelector(".alert");
@@ -23,31 +23,113 @@ const controlPanel = {
   selectBtn: document.getElementById("select-button"),
 };
 
-// Other variables
-const productCollection = {};
+// ** Other variables **
+const productCollection = {}; // general list containing all list-items added to productList without "formed-list" class
 
-let currentList;
-const listCollection = {};
+const listCollection = {}; // collection of lists, e.g. { list1: { product-name-milk: element } }
+let currentList; // name of the list that is currently shown in productList with "formed-list" class, e.g. list1
 
 const popupTimerIDs = [];
 
-// Functions
-const removeElementFromProductList = (e) => {
-  const listElem = e.target.parentElement;
-  if (productList.classList.contains("formed-list")) {
-    listCollection[currentList].delete(listElem);
+// ** Tips **
+// If productList has "formed-list" class then it shows only list-items that are currently in listCollection[currentList] object
+// If productList has "formed-list" class then add-button will add list-item only to listCollection[currentList] object
+
+// If productList has no "formed-list" class then it shows only list-items that are currently in productCollection object
+// If productList has no "formed-list" class then add-button will add list-item only to productCollection object
+
+// ** Functions **
+const addFormedList = () => {
+  productList.classList.add("formed-list");
+  controlPanel.newBtn.classList.remove("disabled");
+  controlPanel.formBtn.classList.add("disabled");
+};
+
+const removeFormedList = () => {
+  productList.classList.remove("formed-list");
+  controlPanel.newBtn.classList.add("disabled");
+  controlPanel.formBtn.classList.remove("disabled");
+};
+
+const isFormedListActive = () => productList.classList.contains("formed-list");
+
+const addListItemToProductList = (productID, productName) => {
+  const listItem = document.createElement("li");
+  listItem.dataset.productKey = productID;
+  listItem.innerHTML = `
+    <span><input id="${productID}" type="checkbox" checked /></span>
+    <label for="${productID}">${productName}</label>
+    <button>✖</button>
+  `;
+
+  listItem.firstElementChild.addEventListener(
+    "click",
+    clickOnCheckboxInListItem
+  );
+  listItem.firstElementChild.firstElementChild.addEventListener(
+    "change",
+    checkFormBtnState
+  );
+  listItem.lastElementChild.addEventListener(
+    "click",
+    removeListItemFromProductList
+  );
+
+  productList.append(listItem);
+
+  if (isFormedListActive()) {
+    listCollection[currentList][productID] = listItem;
   } else {
-    delete productCollection[listElem.dataset.productId];
+    productCollection[productID] = listItem;
+    checkFormBtnState();
   }
+};
 
+const removeListItemFromProductList = (e) => {
+  const listItem = e.target.parentElement;
+
+  // If there is no list-item in both collections then we can remove handlers from it
   if (
-    !Object.values(productCollection).includes(listElem) &&
-    Object.values(listCollection).every((setItem) => !setItem.has(listElem))
+    !Object.hasOwn(productCollection, listItem.dataset.productKey) &&
+    Object.values(listCollection).every(
+      (list) => !Object.hasOwn(list, listItem.dataset.productKey)
+    )
   ) {
-    e.target.removeEventListener("click", removeElementFromProductList);
+    listItem.firstElementChild.removeEventListener(
+      "click",
+      clickOnCheckboxInListItem
+    );
+    listItem.firstElementChild.firstElementChild.removeEventListener(
+      "change",
+      checkFormBtnState
+    );
+    listItem.lastElementChild.removeEventListener(
+      "click",
+      removeListItemFromProductList
+    );
   }
 
-  listElem.remove();
+  listItem.remove();
+
+  if (isFormedListActive()) {
+    delete listCollection[currentList][listItem.dataset.productKey];
+  } else {
+    delete productCollection[listItem.dataset.productKey];
+    checkFormBtnState();
+  }
+};
+
+const checkFormBtnState = () => {
+  if (productList.querySelector("li:has(input:checked)")) {
+    controlPanel.formBtn.classList.remove("disabled");
+  } else {
+    controlPanel.formBtn.classList.add("disabled");
+  }
+};
+
+const clickOnCheckboxInListItem = (e) => {
+  if (e.target !== e.currentTarget) return;
+  e.target.firstElementChild.click();
 };
 
 const showPopup = (popup, index) => {
@@ -62,14 +144,16 @@ const showPopup = (popup, index) => {
   }, 1800);
 };
 
-const hideModal = (e) => {
+const hideModal = () => {
   modal.input.value = "";
   modal.container.hidden = true;
   document.removeEventListener("click", hideModal);
 };
 
-// Listeners
+// ** Listeners **
 productForm.addEventListener("reset", () => productTextInput.focus());
+
+// When we press add-button
 productForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -84,26 +168,17 @@ productForm.addEventListener("submit", (e) => {
 
   productID = "product-name-" + productID.replace(/[^a-z0-9-]/g, "");
 
-  if (Object.hasOwn(productCollection, productID)) {
+  if (
+    (isFormedListActive() &&
+      Object.hasOwn(listCollection[currentList], productID)) ||
+    (!isFormedListActive() && Object.hasOwn(productCollection, productID))
+  ) {
     showPopup(alertPopupForProductInput, 0);
     return;
   }
 
   productTextInput.value = "";
-
-  const productListElement = document.createElement("li");
-  productListElement.dataset.productId = productID;
-  productListElement.innerHTML = `
-    <span><input id="${productID}" type="checkbox" checked /></span>
-    <label for="${productID}">${productName}</label>
-    <button>✖</button>
-  `;
-  productListElement.lastElementChild.addEventListener(
-    "click",
-    removeElementFromProductList
-  );
-  productList.append(productListElement);
-  productCollection[productID] = productListElement;
+  addListItemToProductList(productID, productName);
 });
 
 // ----
@@ -113,14 +188,16 @@ controlPanel.formBtn.addEventListener("click", (e) => {
   modal.container.hidden = false;
   document.addEventListener("click", hideModal);
 });
+
 controlPanel.newBtn.addEventListener("click", () => {
-  controlPanel.newBtn.classList.add("disabled");
-  controlPanel.formBtn.classList.remove("disabled");
-  productList.classList.remove("formed-list");
+  removeFormedList();
+
   productList.replaceChildren(...Object.values(productCollection));
+  checkFormBtnState();
 });
 
 // ----
+// When we press ok-button in modal
 modal.form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -130,14 +207,16 @@ modal.form.addEventListener("submit", (e) => {
   }
 
   currentList = modal.input.value;
-  listCollection[modal.input.value] = new Set(
-    productList.querySelectorAll("li:has(input:checked)")
-  );
+  listCollection[currentList] = {};
+  productList.querySelectorAll("li:has(input:checked)").forEach((listItem) => {
+    listCollection[currentList][listItem.dataset.productKey] = listItem;
+  });
+
   modal.input.value = "";
   modal.container.hidden = true;
-  controlPanel.newBtn.classList.remove("disabled");
-  controlPanel.formBtn.classList.add("disabled");
-  productList.classList.add("formed-list");
+  addFormedList();
 });
+
 modal.form.addEventListener("click", (e) => e.stopPropagation());
+
 modal.cancelBtn.addEventListener("click", hideModal);
